92d5011b34c550b3d91434afae1a7043efc5ec5d
grooshbene <wltn000129@gmail.com>
2017-09-24 03:20:40 +0900

    reservation.js API Done

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,12 +1,39 @@
 function init(app, User, Reservation, randomString){
     app.post("/reservation/create", function (req, res) {
-
+        var reservation = new Reservation({
+            _id : randomString.generate(15),
+            date : req.body.date,
+            time : req.body.time,
+            location : req.body.location
+        });
+        reservation.save(function (err, result) {
+            if(err){
+                console.log("DB Error");
+                res.send(401, "DB Error");
+            }
+            res.send(200, reservation);
+        });
     });
     app.post("/reservation/edit", function (req, res) {
-
+        Reservation.findOneAndUpdate({_id : req.body.id},
+            {date : req.body.date,
+                time : req.body.time,
+                location : req.body.location}, function (err, result) {
+            if(err){
+                console.log("Update Error");
+                res.send(401, "Update Error");
+            }
+            res.send(200, result);
+        });
     });
     app.post("/reservation/cancel", function (req, res) {
-
+        Reservation.findOneAndRemove({_id : req.body.id}, function (err, result) {
+            if(err){
+                console.log("Remove Error");
+                res.send(401, "Remove Error");
+            }
+            res.send(200, result);
+        });
     });
 }
 module.exports = init;
