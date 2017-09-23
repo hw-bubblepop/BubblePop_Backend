231256e715d9664ff6b819f7c024fa3e79b5da98
dudco <dudco1129@edcan.kr>
2017-09-23 15:46:42 +0900

    modules added, User, Payment, HeavenCard, Reservation, Party, Study Schema Added

--- app.js
+++ app.js
@@ -4,11 +4,129 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var randomString = require('randomstring');
+var session = require('express-session');
+var schema = mongoose.Schema();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var UserSchema = new schema({
+    _id : String,
+    thumbnail : String,
+    email : String,
+    password : String,
+    nickname : String,
+    location : Number,
+    heavencard : {
+        type : String,
+        ref : 'heavencards'
+    },
+    payment : [{
+        type : String,
+        ref : "payments"
+    }],
+    reservation : [{
+        type : String,
+        ref : "reservations"
+    }],
+    study : [{
+        type : String,
+        ref : "studies"
+    }],
+    party : [{
+        type : String,
+        ref : "parties"
+    }],
+    friends : [{
+        type : String,
+        ref : "users"
+    }],
+    privateChat : [{
+        type : String,
+        ref : "chats"
+    }],
+    publicChat :[{
+        type : String,
+        ref : "chats"
+    }]
+});
+
+var PaymentSchema = new schema({
+    _id : String,
+    owner : String,
+    exchange : String,
+    number : String
+});
+
+var HeavenCardSchema = new schema({
+    _id : String,
+    description : String,
+    position : String,
+    organization : String,
+    phone : String,
+    email : String,
+    thumbnail : String,
+    balance : Number,
+    cardHistory : [{
+        type : Object
+    }],
+    cardOrder : Object
+});
+
+var ReservationSchema = new schema({
+    _id : String,
+    date : String,
+    time : String,
+    location : String
+});
+
+var PartySchema = new schema({
+    _id : String,
+    title : String,
+    description : String,
+    thumbnail : String,
+    like : Number,
+    comment : [{
+        type : String,
+        ref : "comments"
+    }],
+    date : String
+});
+
+var StudySchema = new schema({
+    _id : String,
+    title : String,
+    description : String,
+    category : Number,
+    location : String,
+    thumbnail : String,
+    like : Number,
+    comment : [{
+        type : String,
+        ref : "comments"
+    }],
+    date : String,
+    member : [{
+        type : String,
+        ref : "users"
+    }],
+    chat : {
+        type : String,
+        ref : "chats"
+    },
+    board : [{
+        type : String,
+        ref : "boards"
+    }],
+    memory : [{
+        type : String,
+        ref : "memories"
+    }]
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
