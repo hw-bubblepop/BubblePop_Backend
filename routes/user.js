188c75eb707598e2f1d897eb0729a529363d8c8e
grooshbene <wltn000129@gmail.com>
2017-09-24 03:31:58 +0900

    user.js API Done

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,12 @@
 function init(app, User, randomString){
     app.post('/user/edit/nickname', function (req, res) {
-
+        User.findOneAndUpdate({_id : req.body.id}, {nickname : req.body.nickname}, function (err, result) {
+            if(err){
+                console.log("DB Error");
+                res.send(401, "DB Error");
+            }
+            res.send(200, result);
+        });
     });
 }
 module.exports = init;
