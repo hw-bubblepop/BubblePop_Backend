ab4475b81e49dec83578d89cb76df5980e57423e
grooshbene <wltn000129@gmail.com>
2017-09-24 02:49:03 +0900

    booth.js /booth/find Done

--- routes/booth.js
+++ routes/booth.js
@@ -1,6 +1,12 @@
 function init(app, Booth, User, randomString){
     app.post('/booth/find', function (req, res) {
-
+        Booth.find({}, function (err, result) {
+            if(err){
+                console.log("DB Error!");
+                res.send(401, "DB Done");
+            }
+            res.send(200, result);
+        })
     });
 }
 module.exports = init;
