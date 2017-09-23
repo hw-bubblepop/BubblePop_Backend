2c0085457438330b8f18c1285436d0ff630e9c73
grooshbene <wltn000129@gmail.com>
2017-09-23 21:48:43 +0900

    auth.js added, router setting Done

--- app.js
+++ app.js
@@ -186,6 +186,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+require('./routes/auth.js')(app, User, randomString);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+function init(app, User, randomString){
+    app.post('/auth/register', function(req, res){
+
+    });
+    app.post('/auth/login', function (req, res) {
+
+    });
+    app.get('/auth/facebook/token', function (req ,res) {
+
+    });
+}
+module.exports = init;
