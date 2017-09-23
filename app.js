7a3e59edb5b9fc86f6731c7c2b89d2e919afa6d0
grooshbene <wltn000129@gmail.com>
2017-09-23 22:20:48 +0900

    payment.js module added, router setting done

--- app.js
+++ app.js
@@ -196,7 +196,8 @@ app.use('/', index);
 app.use('/users', users);
 require('./routes/auth.js')(app, User, randomString);
 require('./routes/user.js')(app, User, randomString);
-require('./routes/heavencard')(app, User, HeavenCard, randomString);
+require('./routes/heavencard.js')(app, User, HeavenCard, randomString);
+require('./routes/payment.js')(app, User,Payment, randomString);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/payment.js
@@ -0,0 +1,15 @@
+function init(app, User, Payment, randomString) {
+    app.post('/pay', function (req, res) {
+
+    });
+    app.post('/payment/add', function (req, res) {
+
+    });
+    app.post('/payment/update', function (req, res) {
+
+    });
+    app.post('/payment/delete', function (req, res) {
+
+    });
+}
+module.exports = init;
