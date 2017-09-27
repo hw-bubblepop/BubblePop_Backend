005eab22a32a77ca8ff447f7a8274fc72e8390dd
grooshbene <wltn000129@gmail.com>
2017-09-28 04:54:33 +0900

    bodyparser enity chagned

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ limit: '5mb', extended: false, parameterLimit: 10000 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
